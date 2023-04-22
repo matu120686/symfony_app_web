7b82a4d188c437a256366dc1783d0f8ec446f556
Matias Olivera <matu120686@gmail.com>
2023-04-22 13:23:14 -0300

    instalo template

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,12 +11,12 @@ class UserController extends AbstractController
 {
     public function index(): JsonResponse
     {
-        $user = new User();
-        $user->setName('John');
-        $user->setLastname('Doe');
-        $user->setEmail('matu@emgil.com');
-        $user->setPassword('123456');
-        $user->setStatus(1);
+        // $user = new User();
+        // $user->setName('John');
+        // $user->setLastname('Doe');
+        // $user->setEmail('matu@emgil.com');
+        // $user->setPassword('123456');
+        // $user->setStatus(1);
         
         return $this->json([
             'message' => 'Welcome to your new controller!',
