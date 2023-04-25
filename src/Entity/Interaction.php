4fb772001d9cf8c1686acd2c1f4e49c1d0a1492d
Matias Olivera <matu120686@gmail.com>
2023-04-24 22:21:04 -0300

    creo entity para post,user,interaction

---
+++ src/Entity/Interaction.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InteractionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InteractionRepository::class)]
+class Interaction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $user_id = null;
+
+    #[ORM\Column]
+    private ?int $post_id = null;
+
+    #[ORM\Column]
+    private ?bool $user_favorite = null;
+
+    #[ORM\ManyToOne(inversedBy: 'interactions')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    #[ORM\ManyToOne(inversedBy: 'interactions')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Post $post = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(int $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getPostId(): ?int
+    {
+        return $this->post_id;
+    }
+
+    public function setPostId(int $post_id): self
+    {
+        $this->post_id = $post_id;
+
+        return $this;
+    }
+
+    public function isUserFavorite(): ?bool
+    {
+        return $this->user_favorite;
+    }
+
+    public function setUserFavorite(bool $user_favorite): self
+    {
+        $this->user_favorite = $user_favorite;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getPost(): ?Post
+    {
+        return $this->post;
+    }
+
+    public function setPost(?Post $post): self
+    {
+        $this->post = $post;
+
+        return $this;
+    }
+}
